a9cf5268ddaee455fcc3fcf1f017716d4b8a42a6
Edinyanga O. Ottoho <elzucky@gmail.com>
2019-03-14 10:43:32 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    if (isset($_FILES['files'])){
-  	 $filename = $_FILES['files']['name'];
-  	 $file_tmp_name = $_FILES['files']['tmp_name'];
-  	 move_uploaded_file($file_tmp_name, "uploads/".$filename);
-    }
-  }
-?>
